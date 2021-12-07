dbb0d0f92fa1d10c91cb4941729368d9e9750fdf
rabello97 <rodrigo.azevedo.rabello@gmail.com>
2021-12-06 23:13:42 -0300

    up css

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header';
 import Contents from './components/Contents';
+import Footer from './components/Footer';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <Contents/>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
