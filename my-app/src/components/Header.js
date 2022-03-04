4db7cb3973d46da31e42eeff6a8e02d531f4c715
rabello97 <rodrigo.azevedo.rabello@gmail.com>
2022-03-03 23:12:22 -0300

    up

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import marca from '../img/marca.svg'
+import React from "react";
+import marca from "../img/marca.svg";
 
-function Header(){
-    return(
-        <header className="header">
-            <img src={marca} alt="lobo"/>
-            <nav>
-                <ul className="header-menu">
-                    <li><a href="#experiencia">experiência</a></li>
-                    <li><a href="#formacao">formação</a></li>
-                    <li><a href="#contato">contato</a></li>
-                </ul>
-            </nav>
-
-        </header>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <img src={marca} alt="lobo" />
+      <nav>
+        <ul className="header-menu">
+          <li>
+            <a href="#experiencia">experiência teste</a>
+          </li>
+          <li>
+            <a href="#formacao">formação teste</a>
+          </li>
+          <li>
+            <a href="#contato">contato teste</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header; 
+export default Header;
